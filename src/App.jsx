a1bf6d4e6c655aa5b227d3c7d4ec510a426700b6
AlexGrigorenco <alpsnhandy@gmail.com>
2024-02-06 22:15:57 +0200

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Layout from "./components/layout/Layout"
+import './styles/App.scss'
 
 export default function App() {
   return (
     <Layout />
+
   )
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+    return ( 
+        <div className="main-loader"><span class="loader"> </span></div>
+     );
+}
+ 
+export default Loader;
