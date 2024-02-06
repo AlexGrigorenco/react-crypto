55557cda6f81f6024400ebe43ddb278af053feb5
AlexGrigorenco <alpsnhandy@gmail.com>
2024-02-06 21:56:49 +0200

    added emulation to getting data from API-server

--- src/components/layout/AppSideBar.jsx
+++ src/components/layout/AppSideBar.jsx
@@ -1,31 +1,33 @@
-import {Layout} from 'antd'
-import { useResize } from '../../hooks/useResize';
+import { Layout, Card } from "antd";
+import { useResize } from "../../hooks/useResize";
 
 const AppSideBar = () => {
-
-  function setSideBarWidth(){
-    if(useResize().width < 768){
-      return '80%'
+  function setSideBarWidth() {
+    if (useResize().width < 768) {
+      return "80%";
     }
-    return '25%'
+    return "25%";
   }
 
-  
-    return ( 
-        <Layout.Sider 
-        width={setSideBarWidth()}
-        breakpoint="md"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-        id='app-sidebar'>
-        Sider
-      </Layout.Sider>
-     );
-}
- 
+  return (
+    <Layout.Sider
+      width={setSideBarWidth()}
+      breakpoint="md"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+      id="app-sidebar"
+    >
+      <Card
+        style={{ margin: 10 }}
+      >
+      </Card>
+    </Layout.Sider>
+  );
+};
+
 export default AppSideBar;
