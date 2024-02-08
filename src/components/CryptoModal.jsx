364fc44414724365b5c41c6e25266e403f3a0797
AlexGrigorenco <alpsnhandy@gmail.com>
2024-02-08 03:37:00 +0200

    added data view in cryptoModal

---
+++ src/components/CryptoModal.jsx
@@ -0,0 +1,111 @@
+import { Modal, Flex, Typography, Tag, Divider, Button } from "antd";
+import { useState } from "react";
+
+const Cryptomodal = ({ data, isOpen, closeModal }) => {
+  const [showMore, setShowMore] = useState(false);
+  console.log(data);
+  return (
+    <Modal
+      style={{ maxWidth: "90%" }}
+      open={isOpen}
+      onCancel={() => closeModal(false)}
+      footer={null}
+    >
+      {data && (
+        <>
+          <Flex align="center">
+            <img
+              width="40"
+              height="40"
+              src={data.icon}
+              alt={data.name}
+              loading="lasy"
+            />
+            <h3
+              style={{
+                marginLeft: "10px",
+                fontSize: "clamp(20px, 1.023rem + 1.14vw, 30px)",
+              }}
+            >
+              {data.name}
+            </h3>
+            <strong
+              style={{
+                fontSize: "clamp(12px, 0.568rem + 0.91vw, 20px)",
+                transform: "translateY(4px)",
+              }}
+            >
+              &nbsp;({data.symbol})
+            </strong>
+          </Flex>
+          <Typography.Text>
+            <b>rank:</b> {data.rank}
+          </Typography.Text>
+          <Divider />
+          <Flex align="center" gap={10} justify="center" wrap="wrap">
+            <Typography.Text style={{ flexShrink: "0" }} strong>
+              1 hour{" "}
+              <Tag color={data.priceChange1h >= 0 ? "green" : "red"}>
+                {data.priceChange1h}%
+              </Tag>
+            </Typography.Text>
+            <Typography.Text style={{ flexShrink: "0" }} strong>
+              1day{" "}
+              <Tag color={data.priceChange1d >= 0 ? "green" : "red"}>
+                {data.priceChange1d}%
+              </Tag>
+            </Typography.Text>
+            <Typography.Text style={{ flexShrink: "0" }} strong>
+              1week{" "}
+              <Tag color={data.priceChange1w >= 0 ? "green" : "red"}>
+                {data.priceChange1w}%
+              </Tag>
+            </Typography.Text>
+          </Flex>
+          <Divider />
+          <Typography.Text>
+            <b>Price:</b> {Number(data.price.toFixed(2)).toLocaleString()}$ /{" "}
+            {data.priceBtc.toPrecision(3)}Btc
+          </Typography.Text>{" "}
+          <br />
+          <Typography.Text>
+            <b>MarKetCap:</b>{" "}
+            {Number(data.marketCap.toFixed()).toLocaleString()}$
+          </Typography.Text>{" "}
+          <br />
+          <Typography.Text>
+            <b>Supply:</b> <span title="avialable">{data.availableSupply}</span>{" "}
+            / <span title="total">{data.totalSupply}</span>
+          </Typography.Text>{" "}
+          <br />
+          <Typography.Text>
+            <b>Volume:</b> {Number(data.volume.toFixed()).toLocaleString()}$
+          </Typography.Text>{" "}
+          <br />
+          <Flex style={{ paddingTop: "20px" }} justify="center">
+            <Button type="primary" onClick={() => setShowMore(!showMore)}>
+              more info
+            </Button>
+          </Flex>
+          {showMore && <div>
+            <Divider/>
+            {data.websiteUrl && <><div>Site: </div><a href={data.websiteUrl} target="_blank" >{data.websiteUrl}</a></>}
+            <br/>
+            {data.redditUrl && <><div>Reddit: </div><a href={data.redditUrl} target="_blank" >{data.redditUrl}</a></>}
+            <br />
+            {data.twitterUrl && <><div>Reddit: </div><a href={data.twitterUrl} target="_blank" >{data.twitterUrl}</a></>}
+            <br />
+            {data.explorers && <>
+            <div>Explorers</div>
+            {data.explorers.map(link => (
+                <a href={link} style={{display:'block', lineHeight: '1.3'}} target="_blank">{link}</a>
+            ))}
+            </>}
+            </div>}
+        </>
+      )}
+    </Modal>
+  );
+};
+
+export default Cryptomodal;
--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,30 +1,28 @@
-import { Layout, Button, Modal } from "antd";
+import { Layout, Button } from "antd";
 import CryptoSelect from "../CryptoSelect";
 import { useState } from "react";
 import CryptoDrawer from "../CryptoDrawer";
+import { useCrypto } from "../../context/crypto-context";
+import CryptoModal from "../CryptoModal";
 
 const AppHeader = () => {
   const [modal, setModal] = useState(false);
   const [drawer, setDrawer] = useState(false);
   const [coin, setCoin] = useState(null);
+  const { crypto } = useCrypto();
 
-  function getCoinFromSelect(value){
-    setCoin(value)
-    setModal(true)
+  function getCoinFromSelect(value) {
+    setCoin(value);
+    setModal(true);
   }
   return (
     <Layout.Header id="app-header">
       <CryptoSelect func={getCoinFromSelect} />
-      <Button onClick={() => setDrawer(true)} type="primary">Add Asset</Button>
+      <Button onClick={() => setDrawer(true)} type="primary">
+        Add Asset
+      </Button>
       <CryptoDrawer isOpen={drawer} closeDrawer={setDrawer} />
-      <Modal
-        title="Basic Modal"
-        open={modal}
-        onCancel={() => setModal(false)}
-        footer={null}
-      >
-        {coin}
-      </Modal>
+      <CryptoModal isOpen={modal} closeModal={setModal} data={crypto.find((c) => c.id === coin)} />
     </Layout.Header>
   );
 };
