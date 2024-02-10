5e8089d59b678b5dfe7914c843c920bdf4ef28e6
AlexGrigorenco <alpsnhandy@gmail.com>
2024-02-10 06:03:19 +0200

    added form in drawer and added validating amount and price

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,14 +1,122 @@
+import { Form, DatePicker, TimePicker, InputNumber, Button } from "antd";
 
-const AddAssetForm = ({coin}) => {
-    
+const AddAssetForm = ({ coin }) => {
+  const [form] = Form.useForm();
 
-    
-    return ( 
-        <form style={{width: '100%'}}>
-    <img src={coin.icon} alt={coin.name} />
-    {coin.name}
-        </form>
-     );
+  function onFinish(values) {
+    console.log("values:", values);
+  }
+
+  function changeValues(changedValue, allValues) {
+    console.log(changedValue);
+    console.log(allValues);
+
+    if(changedValue.amount || changedValue.amount === 0 || changedValue.price || changedValue.price === 0){
+        if (allValues.amount < 0 || allValues.price < 0 || allValues.amount === undefined) {
+            form.setFieldsValue({
+                total: "-",
+            });
+            return;
+        }
+
+        form.setFieldsValue({
+            total: +(changedValue.amount * allValues.price).toFixed(2),
+        });
+    }
 }
- 
+
+
+  return (
+    <Form
+      form={form}
+      onValuesChange={changeValues}
+      name="basic"
+      labelCol={{
+        span: 6,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        width: "100%",
+      }}
+      initialValues={{
+        price: +coin.price.toFixed(2),
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[
+          {
+            required: true,
+            type: "number",
+            min: 0,
+            message: "Please input amount",
+          },
+        ]}
+      >
+        <InputNumber style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item
+        label="Price"
+        name="price"
+        rules={[
+          {
+            required: true,
+            type: "number",
+            min: 0,
+            message: "Please input price",
+          },
+        ]}
+      >
+        <InputNumber style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item label="Total" name="total">
+        <InputNumber disabled style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item
+        label="Date"
+        name="date"
+        rules={[
+          {
+            required: true,
+            message: "Please choise date",
+          },
+        ]}
+      >
+        <DatePicker style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item
+        label="Time"
+        name="time"
+        rules={[
+          {
+            required: true,
+            message: "Please choise time",
+          },
+        ]}
+      >
+        <TimePicker style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Add Asset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
 export default AddAssetForm;
--- src/components/CryptoDrawer.jsx
+++ src/components/CryptoDrawer.jsx
@@ -1,4 +1,4 @@
-import { Button, Drawer } from "antd";
+import { Button, Drawer, Flex } from "antd";
 import { useResize } from "../hooks/useResize";
 import CryptoSelect from "./CryptoSelect";
 import { useState } from "react";
@@ -24,8 +24,12 @@ const CryptoDrawer = ({isOpen, closeDrawer}) => {
       }
 
       function handleClose(){
-        setCoin(null)
         closeDrawer(false)
+        setCoin(null)        
+      }
+
+      function findCoin(coin){
+        setCoin(crypto.find(c => c.id === coin))
       }
 
     return ( 
@@ -41,9 +45,15 @@ const CryptoDrawer = ({isOpen, closeDrawer}) => {
       >
         <Button style={{position: "absolute", top: '10px', right: '10px',}} onClick={() => handleClose()} type="primary">close</Button>
 
-        {coin && <Button style={{display: 'block',}} onClick={() => setCoin(null)} type="primary">change coin</Button>}
+        {coin && <Flex align="center" style={{width: "100%"}} justify="space-between">
+          <Flex align="center">
+            <img width="30" height="30" src={coin.icon} alt={coin.name} loading="lazy" />
+            <h3 style={{marginLeft: '10px', fontSize: 'clamp(20px, 0.932rem + 1.59vw, 30px)'}}>{coin.name}</h3>
+          </Flex>
+          <div className="coin-reset" onClick={() => setCoin(null)}></div>
+          </Flex>}
 
-        {!coin ? <CryptoSelect func={setCoin} /> : <AddAssetForm coin={crypto.find(c => c.id === coin)} />}
+        {!coin ? <CryptoSelect func={findCoin} /> : <AddAssetForm coin={coin} />}
       </Drawer>
      );
 }
