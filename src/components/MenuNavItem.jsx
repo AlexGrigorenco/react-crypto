ebc2ad3a290feb1db0c2d187d7a45e7f9a511973
AlexGrigorenco <alpsnhandy@gmail.com>
2024-02-23 00:31:41 +0200

    completed nav menu

--- src/components/MenuNavItem.jsx
+++ src/components/MenuNavItem.jsx
@@ -1,4 +1,4 @@
-import MenuNavIcon from "./MenuNavIcon";
+import MenuNavIcon from "./MenuNavIcons";
 const MenuNavItem = ({name, iconName}) => {
     return ( <div className="nav-item">
         <MenuNavIcon iconName={iconName} />
