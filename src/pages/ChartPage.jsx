8901800d44668acf7702cb59a2fcafd8d093f12b
AlexGrigorenco <alpsnhandy@gmail.com>
2024-03-04 16:34:41 +0200

    added base line chart

--- src/pages/ChartPage.jsx
+++ src/pages/ChartPage.jsx
@@ -1,6 +1,61 @@
-const ChartPage = () => {
-    return ( <>
-    chart</> );
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
+
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [800, 900, 500, 100, 300, 900, 910],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [900, 700, 200, 500, 100, 300, 900],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  
+  const ChartPage = () => {
+    return <Line options={options} data={data} />;
 }
  
 export default ChartPage;
